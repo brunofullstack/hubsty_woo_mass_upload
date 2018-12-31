36734cbd5218a4b5bea7a0c7a190815844750500
Bruno Ninja <brunofullstack@gmail.com>
2018-12-31 00:07:23 -0200

    key

--- includes/process-csv-data.php
+++ includes/process-csv-data.php
@@ -352,7 +352,7 @@ function wk_mu_process_csv_data( $val, $author_id, $current, $img_folder, $user_
 		foreach ( $attribute_name as $key => $value ) {
 			$product_attributes_arr[] = array(
 				'name' => $attribute_name,
-				'value' => $attribute_data[],
+				'value' => $attribute_data[$key],
 				'position' => 1,
 				'is_visible' => 1,
 				'is_variation' => 1,
